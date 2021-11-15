13b7b48ab67d816e2d7185f7470f20882ef37589
Vanluom <lluom14@gmail.com>
2021-11-15 17:09:25 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner nc=new Scanner(System.in);
+		int i;int j;
+		for (i=1;i<11;i++)
+		{
+		for(j=2;j<11;j++)
+		{
+		System.out.printf("%d x %2d = %d ", j,i,j*i);
+		}
+		System.out.println("/n");
+	        }
+	        
+	    } 
+	
+}
+
+    
+
+
+
+
